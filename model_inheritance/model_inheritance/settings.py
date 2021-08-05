82894c1a1e21fb7b00d7020918ff4724cb341262
Rohit Prashanth Kannam <rk21155@corp.ojas-it.com>
2021-08-05 17:34:18 +0530

    third commit

--- model_inheritance/model_inheritance/settings.py
+++ model_inheritance/model_inheritance/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inheritance'
+    'inheritance',
+    'multitable',
+    'proxy'
 ]
 
 MIDDLEWARE = [
